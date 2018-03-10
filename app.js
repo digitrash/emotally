43ced008923b86b26cdf0183f05a3a60117e678b
digitrash <mike@napofearth.com>
2018-03-09 23:06:59 -0800

    modal form for topic creation

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 'use strict';
 
 /**
- * @constructor
+ *
  * The Topic model
+ * @param {string} text
+ * @param {string} emotion
+ * @constructor
  */
-emo.Topic = function(text, happy, meh, sad) {
+emo.Topic = function(text, emotion) {
     this.text = text || '';
-    this.happy = happy || 0;
-    this.meh = meh || 0;
-    this.sad = sad || 0;
+    this.happy = emotion === 'happy';
+    this.meh = emotion === 'meh';
+    this.sad = emotion === 'sad';
 };
 
 /**
@@ -17,34 +20,40 @@ emo.Topic = function(text, happy, meh, sad) {
  */
 emo.Topic.prototype.vote = function(type) {
     switch (type) {
-        case happy:
+        case 'happy':
             this.happy++;
             break;
-        case meh:
+        case 'meh':
             this.meh++;
             break;
-        case sad:
+        case 'sad':
             this.sad++;
             break;
     }
 };
 
 /**
- * @constructor
+ *
  * The app code
+ * @constructor
  */
 emo.Tally = function() {
     // DOM elements
     this.$add = $('#addTopic');
     this.$reax = $('#reax');
     this.$topics = $('#topics');
+    this.$modalBg = $('#modalBg');
+    this.$addModal = $('#addModal');
+    this.$cancelAddTopic = $('#cancelAddTopic');
+    this.$submitAddTopic = $('#submitAddTopic');
+    this.$topicInput = $('#topicInput');
 
     // data storage
     this.topics = [];
-    this.topics.push(new emo.Topic('The rain in Spain'));
-    this.topics.push(new emo.Topic('Falls mainly on the plain'));
-    this.topics.push(new emo.Topic('I will not eat them on a train'));
-    this.topics.push(new emo.Topic('I will not eat them on a plane'));
+    this.topics.push(new emo.Topic('The rain in Spain', 'happy'));
+    this.topics.push(new emo.Topic('Falls mainly on the plain', 'meh'));
+    this.topics.push(new emo.Topic('I will not eat them on a train', 'sad'));
+    this.topics.push(new emo.Topic('I will not eat them on a plane', 'sad'));
 
     // state
     this.sortedBy = null;
@@ -58,15 +67,35 @@ emo.Tally.prototype.listeners = function() {
     this.$add.on('click', this.showAddDialog.bind(this));
     this.$topics.on('mouseenter', '.topic', this.showReax.bind(this));
     this.$topics.on('mouseleave', '.topic', this.hideReax.bind(this));
+    this.$reax.on('onmouseenter mouseleave', function(e){
+        e.stopPropagation();
+    });
+    this.$cancelAddTopic.on('click', this.hideAddDialog.bind(this));
+    this.$submitAddTopic.on('click', this.addTopic.bind(this));
 };
 
 emo.Tally.prototype.showAddDialog = function() {
-    var topic = prompt("Enter a new topic", "");
-    if (topic) {
-        console.log("new topic", topic);
-        this.topics.push(new emo.Topic(topic));
-        this.updateTopics();
+    this.$modalBg.show();
+    this.$addModal.show();
+};
+
+emo.Tally.prototype.hideAddDialog = function() {
+    this.$topicInput.val('');
+    this.$modalBg.hide();
+    this.$addModal.hide();
+};
+
+emo.Tally.prototype.addTopic = function() {
+    var topic = this.$topicInput.val();
+    if (!topic) {
+        return;
     }
+    var emotion = $('input[name=emo]:checked').val();
+    console.log("new topic/emotion", topic, emotion);
+    this.topics.push(new emo.Topic(topic, emotion));
+    this.$topicInput.val('');
+    this.updateTopics();
+    this.hideAddDialog();
 };
 
 emo.Tally.prototype.updateTopics = function() {
@@ -78,11 +107,14 @@ emo.Tally.prototype.updateTopics = function() {
 };
 
 emo.Tally.prototype.showReax = function(el) {
-    // this.$reax.show();
+    var $e = $(el.currentTarget);
+    var pos = $e.position();
+    this.$reax.css({'top': pos.top+"px", 'left': pos.left + $e.width() - 100+'px'});
+    this.$reax.show();
 };
 
 emo.Tally.prototype.hideReax = function(el) {
-    // this.$reax.hide();
+    this.$reax.hide();
 };
 
 
